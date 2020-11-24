331266c7b1df46c04f81fc4c8f42a62f2e6c75d6
eduard <eduard.shvetsov@anvileight.com>
2020-11-24 20:43:12 +0200

    Clean main

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,5 @@
 import {Component} from '@angular/core';
 
-export interface IPost {
-  title: string;
-  content: string;
-  id?: number;
-}
-
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
