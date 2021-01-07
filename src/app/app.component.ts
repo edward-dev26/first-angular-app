1760ead616f9ea1dbe106038f248c842ee5fffe6
eduard.shvetsov <eduard.shvetsov@anvileight.com>
2021-01-07 20:24:42 +0200

    Program render component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
-import {Component} from '@angular/core';
+import {Component, ComponentFactoryResolver, ViewChild} from '@angular/core';
+import {ModalComponent} from './modal/modal.component';
+import {RefDirective} from './ref.directive';
+import {Meta, Title} from '@angular/platform-browser';
 
 @Component({
   selector: 'app-root',
@@ -7,5 +10,29 @@ import {Component} from '@angular/core';
 })
 
 export class AppComponent {
+  @ViewChild(RefDirective) refDir: RefDirective;
 
+  constructor(
+    private resolver: ComponentFactoryResolver,
+    private title: Title,
+    private meta: Meta
+  ) {
+    title.setTitle('App Component');
+    meta.addTags([
+      {name: 'description', content: 'This is app component'}
+    ]);
+  }
+
+  showModal() {
+    const modalFactory = this.resolver.resolveComponentFactory(ModalComponent);
+
+    this.refDir.containerRef.clear();
+
+    const component = this.refDir.containerRef.createComponent(modalFactory);
+
+    component.instance.title = 'Dynamic modal';
+    component.instance.close.subscribe(() => {
+      this.refDir.containerRef.clear();
+    });
+  }
 }
